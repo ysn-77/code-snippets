2e9c6e8629671400f0a0fecbd08774a7fef9ba20
Ysn <20640494+ysn-77@users.noreply.github.com>
2020-10-30 14:55:05 +0500

    Update random-shell-commands.sh

--- random-shell-commands.sh
+++ random-shell-commands.sh
@@ -6,3 +6,21 @@ python -m pyftpdlib -w
 sudo chown -R $USER ~/.blabla
 
 youtube-dl -x --audio-format mp3 --audio-quality 4
+
+GIT_SET_USER() {
+  echo " setting username: $1";
+  git config user.name "$1";
+
+  echo " setting email: $2";
+  git config user.email "$2";
+}
+
+KILL_BY_PORT_NUMBER() {
+  sudo kill -9 $(sudo lsof -i :$1 -t)
+}
+
+GIT_PRINT_CURRENT_BRANCH() {
+  var=$(git branch | grep -oP '\*\s*\K(\S+)')
+  echo $var
+  #git branch | grep \* | cut -d ' ' -f2}
+}
