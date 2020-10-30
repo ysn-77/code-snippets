c48ea883bcaa8105264ee93e58c28d852d12c9c5
Ysn <20640494+ysn-77@users.noreply.github.com>
2020-10-30 15:00:33 +0500

    Update random-shell-commands.sh

--- random-shell-commands.sh
+++ random-shell-commands.sh
@@ -6,3 +6,6 @@ python -m pyftpdlib -w
 sudo chown -R $USER ~/.blabla
 
 youtube-dl -x --audio-format mp3 --audio-quality 4
+
+# git authors sorted by LOC
+git log --no-merges --pretty=format:%an --numstat | awk '/./ && !author { author = $0; next } author { ins[author] += $1; del[author] += $2 } /^$/ { author = ""; next } END { for (a in ins) { printf "%10d %10d %10d %s\n", ins[a] - del[a], ins[a], del[a], a } }' | sort -rn
